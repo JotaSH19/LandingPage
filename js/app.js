99ebd118ca6c37537e25bbe1abd52a6211e41a14
Jhoan Hernandez <jhoansebastian.hernandez.contractor@bbva.com>
2025-11-11 14:05:37 -0500

    Initial Version

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// Contenido del <script> original
+const menuBtn = document.getElementById('menu-btn');
+const mobileMenu = document.getElementById('mobile-menu');
+
+menuBtn.addEventListener('click', () => {
+  mobileMenu.classList.toggle('hidden');
+});
